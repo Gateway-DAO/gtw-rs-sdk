7391edc479cf10c8639ba531c669bc0071ffb0a2
manish <github.jmtss@gmail.com>
2024-08-29 16:27:08 +0530

    feat: baisc implementation of auth module

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
-mod client;
+use reqwest::Client;
+mod auth;
+pub mod models;
 
-pub use client::ApiClient;
+pub const BASE_URL: &str = "https://dev.api.gateway.tech";
+
+pub struct GTW_API {
+    client: Client,
+    bearer_token: String,
+}
