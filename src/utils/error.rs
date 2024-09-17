f3138ded817edb0c5ea708d3406375ae5ab0ec36
manish <github.jmtss@gmail.com>
2024-09-17 16:50:36 +0530

    chore : improved err handling

--- src/utils/error.rs
+++ src/utils/error.rs
@@ -1,10 +1,16 @@
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-
 pub enum GTWError {
     #[error("Network error: {0}")]
     NetworkError(#[from] reqwest::Error),
+
     #[error("JSON parsing error: {0}")]
     JsonError(#[from] serde_json::Error),
+
+    #[error("API error: Status {status}, Message: {message}")]
+    ApiError { status: u16, message: String },
+
+    #[error("Unexpected error: {0}")]
+    UnexpectedError(String),
 }
