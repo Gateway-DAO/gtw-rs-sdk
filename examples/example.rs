3ab11172b29089db200facb8a351bf43f729eebe
manish <github.jmtss@gmail.com>
2024-09-26 18:13:13 +0530

    fix:  example scropt

--- examples/example.rs
+++ examples/example.rs
@@ -1,5 +1,5 @@
 use dotenv::dotenv;
-use gtw_rs_sdk::GtwApi;
+use gtw_rs_sdk::GtwSdk;
 use std::env;
 use tokio;
 
@@ -8,11 +8,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
     let bearer_token = env::var("BEARER_TOKEN").expect("SCHEMA_URL is not set"); //put your token over here
-    let gtw_api = GtwApi::new(bearer_token)?;
+    let gtw_api = GtwSdk::new(bearer_token)?;
 
     match gtw_api.account_info().await {
         Ok(account_info) => {
-            println!("Account Info: {:?}", account_info.did.unwrap());
+            println!("Account Info: {:?}", account_info.did);
         }
         Err(e) => {
             eprintln!("Failed to get account info: {}", e);
@@ -21,10 +21,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     match gtw_api.update_account_info("hello", "r11manish").await {
         Ok(update_account_info) => {
-            println!(
-                " Update Account Info: {:?}",
-                update_account_info.username.unwrap()
-            );
+            println!(" Update Account Info: {:?}", update_account_info.username);
         }
         Err(e) => {
             eprintln!("Failed to get account info: {}", e);
